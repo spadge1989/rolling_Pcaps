39fd2e8a26f5094791abd892e0df91e0b56d9d40
spadge1989 <spadge1989@hotmail.co.uk>
2019-10-04 09:10:52 +0100

    Update start_pcap.sh

--- start_pcap.sh
+++ start_pcap.sh
@@ -2,5 +2,5 @@
 echo "Starting Packet Capture"
 sudo ifconfig eno1 promisc
 sleep 30
-sudo tcpdump -ni eno1 -w /pcaps/ESS_Fault-%m-%d-%H-%M-%S -G 3600
+sudo tcpdump -ni eno1 -w /pcaps/PcapName-%m-%d-%H-%M-%S -G 3600
 
